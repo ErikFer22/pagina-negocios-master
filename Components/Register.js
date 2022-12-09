a813f0d72157b254d94a3b0a53dd1e04112c4ed2
BennettxD2001 <rbennettito1@gmail.com>
2022-12-08 18:11:46 -0600

    Cambios Bennett

--- Components/Register.js
+++ Components/Register.js
@@ -27,7 +27,93 @@ const Register = () => {
     };
 
     return (
-        
+        <Container>
+            <Row style={{ justifyContent: 'center' }}>
+
+                <Col md="5">
+
+                    <Form onSubmit={click}>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+
+                        <Form.Label><h4>Crear una cuenta</h4></Form.Label>
+                        <Form.Group className="mb-3" controlId="name">
+                            <Form.Label style={{ fontSize: '21px' }}> Nombre</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                minLength={3}
+                                placeholder="Ingresa tu nombre"
+                                name="name"
+                                value={Register.name}
+                                onChange={onChangeRegister}
+                            />
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group className="mb-3" controlId="last_name">
+                            <Form.Label style={{ fontSize: '21px' }}>Apellidos</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                minLength={3}
+                                placeholder="Ingresa tus apellidos"
+                                name="last_name"
+                                value={Register.last_name}
+                                onChange={onChangeRegister}
+                            />
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label style={{ fontSize: '21px' }}>Correo</Form.Label>
+                            <Form.Control
+                                required
+                                type="email"
+                                minLength={5}
+                                placeholder="nombre-de-usuario@gmail.com"
+                                autoFocus
+                                name="email"
+                                value={Register.email}
+                                onChange={onChangeRegister}
+                            />
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label style={{ fontSize: '21px' }}>Contraseña</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                maxLength="12"
+                                minLength={8}
+                                placeholder="Ingresa tu contraseña"
+                                name="password"
+                                value={Register.password}
+                                onChange={onChangeRegister}
+                            />
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group className="mb-3" controlId="address">
+                            <Form.Label style={{ fontSize: '21px' }}>Dirección</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                minLength={7}
+                                placeholder="Ingresa tu domicilio"
+                                name="address"
+                                value={Register.address}
+                                onChange={onChangeRegister}
+                            />
+                        </Form.Group>
+                        <br></br>
+                        <Button style={{ justifyContent: 'flex-end' }} className='buttonSeeProducts' type='submit'> Registrarse </Button>
+                        <br></br>
+                        <br></br>
+                    </Form>
+                </Col>
+            </Row>
+
+        </Container>
     );
 }
 
