ddc65cb2a5bbdfea136a074bd8dcf9a0caef8e90
BennettxD2001 <rbennettito1@gmail.com>
2022-12-08 18:13:15 -0600

    Cambios Bennett

--- Components/UserProfile.js
+++ Components/UserProfile.js
@@ -24,7 +24,60 @@ function GridComplexExample() {
     disableFalse();
   }
   return (
+    <Form>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     
+      <Col className="mb-3 mx-5" >
+      <Col>
+                  <img
+                        width="100"
+                        height="100"
+                        alt='GMAIL'
+                        title='GMAIL'
+                        src="/react-example/Images/Perfil.JPG">
+                  </img>
+      </Col>
+      <center>
+        <h2>Mi perfil</h2>
+        <br></br>
+      <Form.Group as={Col} className="col-md-4" controlId="formGridName">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control placeholder={editUsuario.nombre}  disabled={disable}/>
+      </Form.Group>
+
+  
+      
+        <Form.Group as={Col} className="col-md-4" controlId="formGridLastname1">
+          <Form.Label>Apellido paterno</Form.Label>
+          <Form.Control placeholder={editUsuario.apellidoP} disabled={disable}/>
+        </Form.Group>
+     
+
+     
+        <Form.Group as={Col} className="col-md-4" controlId="formGridLastname2">
+          <Form.Label>Apellido materno</Form.Label>
+          <Form.Control placeholder={editUsuario.apellidoM}  disabled={disable}/>
+        </Form.Group>
+        <Form.Group as={Col}  className="col-md-4" controlId="formGridEmail">
+          <Form.Label>Correo</Form.Label>
+          <Form.Control
+            placeholder={editUsuario.correo}
+            disabled={disable} />
+        </Form.Group>
+        <Form.Group as={Col} className="col-md-4" controlId="formGridAddress">
+        <Form.Label>Dirección</Form.Label>
+        <Form.Control placeholder={editUsuario.direccion} disabled={disable} />
+        </Form.Group>
+        </center>
+      </Col>
+    
+    </Form>
 
 
   );
